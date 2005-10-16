a72ba4f2ea14953ecc653744f31dcdcf32a944cb
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2005-10-16 18:45:02 +0000

    Implement greatest common divisor

--- trunk/OpenFDM/src/OpenFDM/Math.h
+++ trunk/OpenFDM/src/OpenFDM/Math.h
@@ -52,7 +52,8 @@ using std::min;
 using std::max;
 
 template<typename T>
-inline int sign(T val)
+inline int
+sign(T val)
 {
   if (val <= -Limits<T>::min())
     return -1;
@@ -62,7 +63,8 @@ inline int sign(T val)
 }
 
 template<typename S, typename T>
-inline S interpolate(T x, T x0, const S& y0, T x1, const S& y1)
+inline S
+interpolate(T x, T x0, const S& y0, T x1, const S& y1)
 {
   // If called in the wrong order, simply call with the correct order ...
   if (x1 < x0)
@@ -78,6 +80,31 @@ inline S interpolate(T x, T x0, const S& y0, T x1, const S& y1)
   } else {
     return y1;
   }
+}
+
+/// Compute the greatest common divisor of a and b
+/// Floating point version, might provide problems with roundoff ...
+inline
+real_type
+greatestCommonDivisor(real_type a_, real_type b_)
+{
+  real_type a = fabs(a_);
+  real_type b = fabs(b_);
+
+  real_type eps = 64*max(a, b)*Limits<real_type>::epsilon();
+  unsigned opcount = 1;
+  while (eps*opcount < b) {
+    real_type r = fmod(a, b);
+    a = b;
+    b = r;
+    ++opcount;
+  }
+
+  // should return a, but make it a natural numbered quotient as much
+  // as possible
+//   std::cout << a - a_/floor(a_/a + 0.5) << std::endl;
+//   return a;
+  return fabs(a_)/floor(fabs(a_)/a + 0.5);
 }
 
 } // namespace OpenFDM
