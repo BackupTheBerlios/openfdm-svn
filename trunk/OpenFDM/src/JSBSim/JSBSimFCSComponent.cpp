0f9ce10e828ddad0d3560458f3a6bc2332e51a26
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2005-12-09 18:30:10 +0000

    Add missing file

---
+++ trunk/OpenFDM/src/JSBSim/JSBSimFCSComponent.cpp
@@ -0,0 +1,41 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2005 Mathias Froehlich 
+ *
+ */
+
+#include <OpenFDM/ModelGroup.h>
+#include <OpenFDM/Gain.h>
+
+#include "JSBSimFCSComponent.h"
+
+namespace OpenFDM {
+
+JSBSimFCSComponent::JSBSimFCSComponent(const std::string& name, bool normOut)
+{
+  mModelGroup = new ModelGroup(name);
+
+  mModelGroup->setNumOutputPorts(1);
+  getOutputPort()->setName("Output");
+
+  if (normOut) {
+    mModelGroup->setNumOutputPorts(2);
+    getOutputNormPort()->setName("OutputNorm");
+  }
+}
+
+JSBSimFCSComponent::~JSBSimFCSComponent(void)
+{
+}
+
+Port*
+JSBSimFCSComponent::getOutputPort(void)
+{
+  return mModelGroup->getOutputPort(0);
+}
+
+Port*
+JSBSimFCSComponent::getOutputNormPort(void)
+{
+  return mModelGroup->getOutputPort(1);
+}
+
+} //namespace OpenFDM
