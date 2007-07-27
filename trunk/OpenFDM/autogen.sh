689bfce3792ab3a10ff0155a4bb61928aaa838a8
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2007-07-27 18:08:44 +0000

    use autoreconf

--- trunk/OpenFDM/autogen.sh
+++ trunk/OpenFDM/autogen.sh
@@ -1,7 +1,18 @@
-rm -rf autom4te.cache
-rm -f src/OpenFDM/OpenFDMConfig.h.in src/OpenFDM/config.h.in
-touch src/OpenFDM/OpenFDMConfig.h.in src/OpenFDM/config.h.in
-libtoolize
-aclocal
-autoconf
-automake --add-missing
+#! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
+rm -f $srcdir/src/OpenFDM/OpenFDMConfig.h.in $srcdir/src/OpenFDM/config.h.in
+touch $srcdir/src/OpenFDM/OpenFDMConfig.h.in $srcdir/src/OpenFDM/config.h.in
+# libtoolize
+# aclocal
+# autoconf
+# automake --add-missing
+autoreconf -v --install || exit 1
+cd $ORIGDIR || exit $?
+
+$srcdir/configure --enable-maintainer-mode "$@"
