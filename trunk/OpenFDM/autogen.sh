729658e5ff8c2c9608019343880a4604a6c721c3
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2007-12-16 08:13:19 +0000

    Do not remove OpenFDMConfig.h.in

--- trunk/OpenFDM/autogen.sh
+++ trunk/OpenFDM/autogen.sh
@@ -6,8 +6,8 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
-rm -f $srcdir/src/OpenFDM/OpenFDMConfig.h.in $srcdir/src/OpenFDM/config.h.in
-touch $srcdir/src/OpenFDM/OpenFDMConfig.h.in $srcdir/src/OpenFDM/config.h.in
+#rm -f $srcdir/src/OpenFDM/config.h.in
+#touch $srcdir/src/OpenFDM/config.h.in
 # libtoolize
 # aclocal
 # autoconf
