85ca954e8be5f5e020165c363e92ad32302aba05
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-02-03 08:54:14 +0000

    Add safe_min(), min_value(), max_value().

--- trunk/OpenFDM/src/OpenFDM/Limits.h
+++ trunk/OpenFDM/src/OpenFDM/Limits.h
@@ -17,7 +17,17 @@ namespace OpenFDM {
 /// So this is one of the three places where modifications for special
 /// real types must be made (@see Types.h, @see Math.h).
 template<typename T>
-struct Limits : public std::numeric_limits<T> {};
+struct Limits : public std::numeric_limits<T> {
+   /// smalles value so that 1/safe_min is still finite
+   static T safe_min()
+   { return std::max((T(1) + std::numeric_limits<T>::epsilon())/std::numeric_limits<T>::max(), std::numeric_limits<T>::min()); }
+   /// minimum value this type can represent
+   static T min_value()
+   { return std::min(std::numeric_limits<T>::min(), -std::numeric_limits<T>::max()); }
+   /// maximum value this type can represent
+   static T max_value()
+   { return std::numeric_limits<T>::max(); }
+};
 
 } // namespace OpenFDM
 
