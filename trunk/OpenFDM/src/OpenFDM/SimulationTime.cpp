76bc250fd0fef224ff41942faaf45c6314a13220
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-02-20 18:06:53 +0000

    Add SimulationTime Model.

---
+++ trunk/OpenFDM/src/OpenFDM/SimulationTime.cpp
@@ -0,0 +1,48 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2006 Mathias Froehlich 
+ *
+ */
+
+#include "SimulationTime.h"
+#include "ModelVisitor.h"
+
+namespace OpenFDM {
+
+BEGIN_OPENFDM_OBJECT_DEF(SimulationTime, Model)
+  END_OPENFDM_OBJECT_DEF
+
+SimulationTime::SimulationTime(const std::string& name) :
+  Model(name)
+{
+  setNumOutputPorts(1);
+  setOutputPort(0, "output", this, &SimulationTime::getOutputValue);
+}
+
+SimulationTime::~SimulationTime(void)
+{
+}
+
+void
+SimulationTime::accept(ModelVisitor& visitor)
+{
+  visitor.apply(*this);
+}
+
+bool
+SimulationTime::init()
+{
+  return Model::init();
+}
+
+void
+SimulationTime::output(const TaskInfo& taskInfo)
+{
+  mOutputValue = taskInfo.getTime();
+}
+
+const real_type&
+SimulationTime::getOutputValue(void) const
+{
+  return mOutputValue;
+}
+
+} // namespace OpenFDM
---
+++ trunk/OpenFDM/src/OpenFDM/SimulationTime.h
@@ -0,0 +1,34 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2006 Mathias Froehlich 
+ *
+ */
+
+#ifndef OpenFDM_SimulationTime_H
+#define OpenFDM_SimulationTime_H
+
+#include "Model.h"
+
+namespace OpenFDM {
+
+class SimulationTime : public Model {
+  OPENFDM_OBJECT(SimulationTime, Model);
+public:
+  SimulationTime(const std::string& name);
+  virtual ~SimulationTime(void);
+
+  /// Double dispatch helper for the system visitor
+  virtual void accept(ModelVisitor& visitor);
+  /// Double dispatch helper for the system visitor
+//   virtual void accept(ConstModelVisitor& visitor) const;
+
+  virtual bool init(void);
+  virtual void output(const TaskInfo&);
+
+  const real_type& getOutputValue(void) const;
+
+private:
+  real_type mOutputValue;
+};
+
+} // namespace OpenFDM
+
+#endif
