14458345b37a030ad65dc4fac4aaf95679d628c6
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-09-11 16:28:48 +0000

    Merge changes from trunk

--- branches/OpenFDM-StateSeparation/src/OpenFDM/LogStream.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/LogStream.h
@@ -6,192 +6,71 @@
 #define OpenFDM_LogStream_H
 
 #include <iosfwd>
+// FIXME: because of using std::endl;
 #include <ostream>
 
 namespace OpenFDM {
 
-namespace Log {
-
-enum Category {
-  ArtBody          = 1,
-  MultiBody        = ArtBody << 1,
-  TimeStep         = MultiBody << 1,
-  BoundCheck       = TimeStep << 1,
-  Environment      = BoundCheck << 1,
-  Frame            = Environment << 1,
-  Initialization   = Frame << 1,
-  NewtonMethod     = Initialization << 1,
-  Misc             = NewtonMethod << 1,
-  Model            = Misc << 1,
-  Schedule         = Model << 1,
-  Assert           = ~0
-};
-
-enum Priority {
-  Error            = 0,
-  Warning          = Error + 1,
-  Info             = Warning + 1,
-  Debug            = Info + 1,
-  Debug1           = Debug + 1,
-  Debug2           = Debug1 + 1,
-  Debug3           = Debug2 + 1
-};
-
-template<Category category, Priority priority>
-class LogFactory;
-
-template<typename charT, typename traits = std::char_traits<charT> >
-class LogProxy;
-
-template<typename charT, typename traits = std::char_traits<charT> >
-class LogSink;
-
-template<class charT, class traits>
-class LogProxy {
-public:
-  typedef std::ios_base                    ios_base_type;
-  typedef std::basic_ios<charT,traits>     basic_ios_type;
-  typedef std::basic_ostream<charT,traits> basic_ostream_type;
-
-  template<typename T>
-  LogProxy operator<<(const T& value)
-  { if (mStream) (*mStream) << value; return *this; }
-  LogProxy operator<<(ios_base_type& (*mptr)(ios_base_type&))
-  { if (mStream) mptr(*mStream); return *this; }
-  LogProxy operator<<(basic_ios_type& (*mptr)(basic_ios_type&))
-  { if (mStream) mptr(*mStream); return *this; }
-  LogProxy operator<<(basic_ostream_type& (*mptr)(basic_ostream_type&))
-  { if (mStream) mptr(*mStream); return *this; }
-
-private:
-  template<Category category, Priority priority>
-  friend class LogFactory;
-
-  LogProxy(const LogProxy& lp) : mStream(lp.mStream) {}
-  LogProxy(basic_ostream_type* s) : mStream(s) {}
-  LogProxy& operator=(const LogProxy&);
-
-  // A pointer to a stream we want to do output to.
-  basic_ostream_type* mStream;
-};
-
-template<class charT, class traits>
-class LogSink {
-public:
-  typedef std::ios_base                    ios_base_type;
-  typedef std::basic_ios<charT,traits>     basic_ios_type;
-  typedef std::basic_ostream<charT,traits> basic_ostream_type;
-
-  template<typename T>
-  LogSink operator<<(const T&)
-  { return *this; }
-  LogSink operator<<(ios_base_type& (*mptr)(ios_base_type&))
-  { return *this; }
-  LogSink operator<<(basic_ios_type& (*mptr)(basic_ios_type&))
-  { return *this; }
-  LogSink operator<<(basic_ostream_type& (*mptr)(basic_ostream_type&))
-  { return *this; }
-
-private:
-  template<Category category, Priority priority>
-  friend class LogFactory;
-
-  LogSink(const LogSink&) {}
-  LogSink(void) {}
-  LogSink& operator=(const LogSink&);
-};
-
 class Logger {
 public:
+  enum Category {
+    ArtBody          = 1,
+    MultiBody        = ArtBody << 1,
+    TimeStep         = MultiBody << 1,
+    BoundCheck       = TimeStep << 1,
+    Environment      = BoundCheck << 1,
+    Frame            = Environment << 1,
+    Initialization   = Frame << 1,
+    NewtonMethod     = Initialization << 1,
+    Misc             = NewtonMethod << 1,
+    Model            = Misc << 1,
+    Schedule         = Model << 1,
+    Assert           = ~0
+  };
+  
+  enum Priority {
+    Error            = 0,
+    Warning          = Error + 1,
+    Info             = Warning + 1,
+    Debug            = Info + 1,
+    Debug1           = Debug + 1,
+    Debug2           = Debug1 + 1,
+    Debug3           = Debug2 + 1
+  };
+  
   static void setCategoryEnable(Category category, bool enable = true);
   static void setCategoryDisable(Category category);
   static void setPriority(Priority priority);
 
-  bool enabled(Category category, Priority priority) const
+  static inline bool
+  getStaticEnabled(Category category, Priority priority)
   {
-    return ((category & mCategory) && (priority <= mPriority))
-      || (priority == Error);
+#if defined(NDEBUG) || defined(_NDEBUG)
+    // In the NDEBUG case, give the compilers optimizer a chance to
+    // completely remove the code.
+    if (Info <= priority)
+      return false;
+#endif
+    return Instance()->getEnabled(category, priority);
   }
-
-  std::basic_ostream<char>* getStream(void)
-  { return mStream; }
+  
+  static std::ostream& getStream(Priority priority);
 
 protected:
   static Logger* Instance(void);
+  bool getEnabled(Category category, Priority priority);
 
 private:
-  Logger(std::basic_ostream<char>* stream = 0);
+  Logger(std::ostream* stream = 0);
 
-  std::basic_ostream<char>* mStream;
+  std::ostream* mStream;
   unsigned mCategory;
   int mPriority;
 };
 
-#if defined(NDEBUG)
-
-template<Category category, Priority priority>
-class LogFactory : public Logger {
-public:
-  typedef LogSink<char> proxy_type;
-  static proxy_type
-  CreateInstance(void)
-  { return proxy_type(); }
-};
-
-template<Category category>
-class LogFactory<category,Error> : public Logger {
-public:
-  typedef LogProxy<char> proxy_type;
-  static proxy_type
-  CreateInstance(void)
-  {
-    Logger* logger = Instance();
-    if (logger->enabled(category, Error))
-      return proxy_type(logger->getStream());
-    else
-      return proxy_type(0);
-  }
-};
-
-template<Category category>
-class LogFactory<category,Warning> : public Logger {
-public:
-  typedef LogProxy<char> proxy_type;
-  static proxy_type
-  CreateInstance(void)
-  {
-    Logger* logger = Instance();
-    if (logger->enabled(category, Warning))
-      return proxy_type(logger->getStream());
-    else
-      return proxy_type(0);
-  }
-};
-
-#else /* defined(NDEBUG) */
-
-template<Category category, Priority priority>
-class LogFactory : public Logger {
-public:
-  static LogProxy<char>
-  CreateInstance(void)
-  {
-    Logger* logger = Instance();
-    if (logger->enabled(category, priority))
-      return LogProxy<char>(logger->getStream());
-    else
-      return LogProxy<char>(0);
-  }
-};
-
-#endif /* else defined(NDEBUG) */
-
-} // namespace Log
-
 #define Log(category, priority) \
-  Log::LogFactory<Log::category,Log::priority>::CreateInstance()
-
-using Log::Logger;
+if (Logger::getStaticEnabled(Logger::category, Logger::priority)) \
+  Logger::getStream(Logger::priority)
 
 using std::endl;
 
