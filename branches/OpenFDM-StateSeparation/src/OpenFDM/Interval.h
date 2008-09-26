37b23a3e9c0828941fcf72bacbbfa90c4da5af76
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-09-26 04:13:33 +0000

    Add Interval implementation.

---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/Interval.h
@@ -0,0 +1,53 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2007-2008 Mathias Froehlich 
+ *
+ */
+
+#ifndef OpenFDM_Interval_H
+#define OpenFDM_Interval_H
+
+#include "Limits.h"
+
+namespace OpenFDM {
+
+template<typename T>
+class Interval {
+public:
+  typedef T value_type;
+
+  Interval() :
+    mBegin(Limits<value_type>::max()), mEnd(-Limits<value_type>::max())
+  { }
+  Interval(const value_type& begin, const value_type& end) :
+    mBegin(begin), mEnd(end)
+  { }
+
+  void setBegin(const value_type& begin) { mBegin = begin; }
+  const value_type& getBegin() const { return mBegin; }
+
+  void setEnd(const value_type& end) { mBegin = end; }
+  const value_type& getEnd() const { return mEnd; }
+
+  bool valid() const
+  { return mBegin <= mEnd; }
+  value_type length() const
+  { return mEnd - mBegin; }
+
+  bool isLeftOf(const value_type& value) const
+  { return value <= mBegin; }
+  bool isStrictlyLeftOf(const value_type& value) const
+  { return value < mBegin; }
+  bool isRightOf(const value_type& value) const
+  { return mEnd <= value; }
+  bool isStrictlyRightOf(const value_type& value) const
+  { return mEnd < value; }
+  bool contains(const value_type& value) const
+  { return mBegin <= value && value <= mEnd; }
+
+private:
+  value_type mBegin;
+  value_type mEnd;
+};
+
+} // namespace OpenFDM
+
+#endif
