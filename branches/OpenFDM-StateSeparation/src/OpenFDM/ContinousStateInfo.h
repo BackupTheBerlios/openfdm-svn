b47e03812abced0cd0b291132fa45aaeb160c2d6
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-10-02 05:18:58 +0000

    Add some properties to continous states.

--- branches/OpenFDM-StateSeparation/src/OpenFDM/ContinousStateInfo.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/ContinousStateInfo.h
@@ -14,12 +14,39 @@ class LeafContext;
 
 class ContinousStateInfo : public StateInfo {
 public:
+  ContinousStateInfo(bool isTrimable = false) :
+    mRelTol(1e-3),
+    mAbsTol(1e-5),
+    mIsTrimable(isTrimable)
+  { }
+
+  void setIsTrimable(bool isTrimable)
+  { mIsTrimable = isTrimable; }
+  bool getIsTrimable() const
+  { return mIsTrimable; }
+
+  void setRelTol(const real_type& relTol)
+  { mRelTol = relTol; }
+  const real_type& getRelTol() const
+  { return mRelTol; }
+
+  void setAbsTol(const real_type& absTol)
+  { mAbsTol = absTol; }
+  const real_type& getAbsTol() const
+  { return mAbsTol; }
+
   ContinousStateValue* newStateValue(const LeafContext& leafContext) const
   { return newStateValueImplementation(leafContext); }
 
 protected:
   virtual ~ContinousStateInfo() {}
   virtual ContinousStateValue* newStateValueImplementation(const LeafContext&) const = 0;
+private:
+  real_type mRelTol;
+  real_type mAbsTol;
+  bool mIsTrimable;
+//   unsigned char mErrorIndex;
+//   unsigned char mDifferentialEquationOrder;
 };
 
 } // namespace OpenFDM
