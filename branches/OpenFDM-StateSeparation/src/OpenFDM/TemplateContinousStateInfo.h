283101ede27ac7356ef13c82b510de648e16acc2
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-10-03 14:48:27 +0000

    Make ContinousStateValues really work. Remove the now unneeded MatrixStateValue.

--- branches/OpenFDM-StateSeparation/src/OpenFDM/TemplateContinousStateInfo.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/TemplateContinousStateInfo.h
@@ -6,14 +6,63 @@
 #define OpenFDM_TemplateContinousStateInfo_H
 
 #include "ContinousStateInfo.h"
-#include "MatrixStateValue.h"
+#include "ContinousStateValue.h"
 #include "TemplateStateInfo.h"
 
 namespace OpenFDM {
 
 template<typename T>
-class TemplateContinousStateInfo : 
-    public TemplateStateInfo<T, ContinousStateInfo, MatrixStateValue> {
+class TemplateContinousStateInfo : public ContinousStateInfo {
+public:
+  typedef T value_type;
+
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+
+//   reference getValueReference(V* value) const
+  reference getValueReference(StateValue* value) const
+  {
+    OpenFDMAssert(value);
+    OpenFDMAssert(dynamic_cast<Value*>(value));
+    return static_cast<Value*>(value)->mValue;
+  }
+//   const_reference getValueReference(const V* value) const
+  const_reference getValueReference(const StateValue* value) const
+  {
+    OpenFDMAssert(value);
+    OpenFDMAssert(dynamic_cast<const Value*>(value));
+    return static_cast<const Value*>(value)->mValue;
+  }
+
+  reference getValue(TemplateValueVector<ContinousStateInfo,ContinousStateValue>& valueVector) const
+  { return getValueReference(valueVector.getValue(*this)); }
+  const_reference getValue(const TemplateValueVector<ContinousStateInfo,ContinousStateValue>& valueVector) const
+  { return getValueReference(valueVector.getValue(*this)); }
+
+protected:
+  struct Value : public ContinousStateValue {
+    value_type mValue;
+
+    void resize(const Size& size)
+    { mValue.resize(size(0), size(1)); }
+    
+    const Matrix& getMatrix() const
+    { return mValue; }
+    Matrix& getMatrix()
+    { return mValue; }
+    void setMatrix(const Matrix& matrix)
+    { OpenFDMAssert(size(matrix) == size(mValue)); mValue = matrix; }
+    
+    virtual void setValue(const StateStream& stateStream)
+    { stateStream.readSubState(mValue); }
+    virtual void getValue(StateStream& stateStream) const
+    { stateStream.writeSubState(mValue); }
+    virtual LinAlg::size_type getNumStates() const
+    { return rows(mValue)*cols(mValue); }
+  };
+
+  virtual Value* newStateValueImplementation(const LeafContext&) const
+  { return new Value; }
 };
 
 } // namespace OpenFDM
