481783f64ade82e10533ac990c70161ad8281ba4
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-10-13 04:21:12 +0000

    Add the mobile root joint to the list of compiled objects. M    MobileRootJoint.h M    MobileRootJoint.cpp

--- branches/OpenFDM-StateSeparation/src/OpenFDM/MobileRootJoint.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/MobileRootJoint.h
@@ -11,94 +11,26 @@
 #include "Matrix.h"
 #include "Quaternion.h"
 #include "Inertia.h"
-#include "Frame.h"
-#include "RigidBody.h"
-#include "Planet.h"
-#include "Joint.h"
-#include "Environment.h"
+#include "RootJoint.h"
 
 namespace OpenFDM {
 
-class ModelVisitor;
-class MobileRootJointFrame;
-
-class MobileRootJoint
-  : public Joint {
+class MobileRootJoint : public RootJoint {
+  OPENFDM_OBJECT(MobileRootJoint, RootJoint);
 public:
   MobileRootJoint(const std::string& name);
-  virtual ~MobileRootJoint(void);
+  virtual ~MobileRootJoint();
 
-  /// Double dispatch helper for the multibody system visitor
-  virtual void accept(ModelVisitor& visitor);
-  /// Double dispatch helper for the multibody system visitor
-//   virtual void accept(ConstModelVisitor& visitor) const;
-
-  virtual bool init(void);
-
-  virtual void recheckTopology(void);
-
-  /// Gets the relative velocity.
-  const Vector6& getRelVel(void) const;
-  /// Set the relative velocity.
-  void setRelVel(const Vector6& vel);
-  /// Set the relative velocity.
-  void setLinearRelVel(const Vector3& vel);
-  /// Set the relative velocity.
-  void setAngularRelVel(const Vector3& vel);
-
-  /// Gets the relative velocity derivative.
-  const Vector6& getRelVelDot(void) const;
-
-  /// Get the reference position.
-  const Vector3& getRefPosition(void) const;
-  /// Set the reference position.
-  void setRefPosition(const Vector3& p);
-
-  /// Get the reference orientation.
-  const Quaternion& getRefOrientation(void) const;
-  /// Set the reference orientation.
-  void setRefOrientation(const Quaternion& o);
-
-  /// Get the geodetic position.
-  Geodetic getGeodPosition(void) const;
-  /// Set the geodetic position.
-  void setGeodPosition(const Geodetic& geod);
-  /// Get orientation wrt the geodetic hl frame.
-  Quaternion getGeodOrientation(void) const;
-
-  Vector4 getQDot(void) const;
-  Vector3 getPosDot(void) const;
-
-  /** Plugin function for the articulated body algorithm.
-   */
-  virtual void jointArticulation(SpatialInertia& artI, Vector6& artF,
-                                 const SpatialInertia& outI,
-                                 const Vector6& outF);
-
-
-  /** Plugin function for the state propagation.
-   */
-  virtual void setState(const StateStream& state);
-  /** Plugin function for the state propagation.
-   */
-  virtual void getState(StateStream& state) const;
-  /** Plugin function for the state propagation.
-   */
-  virtual void getStateDeriv(StateStream& state);
-
-protected:
-  virtual void setEnvironment(Environment* environment);
-
-private:
-  /// The commonly used gravity model from the environment class
-  /// It is initialized at the init() call
-  SharedPtr<const Gravity> mGravity;
-
-  /// The frame of the mobile root
-  SharedPtr<MobileRootJointFrame> mFrame;
-
-  /// The environment pointer
-  SharedPtr<Environment> mEnvironment;
+  virtual void velocity(const Task&, const ContinousStateValueVector& states,
+                        PortValueList& portValues) const;
+  virtual void articulation(const Task&, const ContinousStateValueVector&,
+                            PortValueList& portValues) const;
+  virtual void acceleration(const Task&, const ContinousStateValueVector&,
+                            PortValueList& portValues) const;
+  virtual void derivative(const DiscreteStateValueVector&,
+                          const ContinousStateValueVector&,
+                          const PortValueList& portValues,
+                          ContinousStateValueVector&) const;
 };
 
 } // namespace OpenFDM
