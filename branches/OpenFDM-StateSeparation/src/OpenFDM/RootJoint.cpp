d47f3046d6ebdc02cb6b0675cd66af4643e18026
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-10-21 21:02:42 +0000

    Initialize root rotational velocity.

--- branches/OpenFDM-StateSeparation/src/OpenFDM/RootJoint.cpp
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/RootJoint.cpp
@@ -14,7 +14,8 @@ BEGIN_OPENFDM_OBJECT_DEF(RootJoint, Interact)
   END_OPENFDM_OBJECT_DEF
 
 RootJoint::RootJoint(const std::string& name) :
-  Interact(name)
+  Interact(name),
+  mAngularBaseVelocity(Vector3::zeros())
 {
 }
 
