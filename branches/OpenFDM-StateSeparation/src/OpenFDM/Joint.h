ce10d827ee2992f8d7d8f30111be05a0b4016a0e
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-11-21 12:27:39 +0000

    Some thought on joints.
    M    Joint.h

--- branches/OpenFDM-StateSeparation/src/OpenFDM/Joint.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/Joint.h
@@ -18,6 +18,13 @@
 
 namespace OpenFDM {
 
+// May be each joint can be a root joint.
+// A joint with one link is a root joint?
+// The given position is then meant to be in the root coordinate system?
+// That is: if connected to a parent, it is the designPosition wrt root joint
+// If not connected, in the root coordinate system of the simulation?
+// It would be nice if each joint is lockable anyway
+// May be then unify all that joint stuff in a common framework here??
 class Joint : public MechanicNode {
   OPENFDM_OBJECT(Joint, MechanicNode);
 public:
