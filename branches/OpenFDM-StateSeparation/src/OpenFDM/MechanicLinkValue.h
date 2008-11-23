61ba23bdfed65836993369a737adfe04040a6bfe
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-11-23 20:28:32 +0000

    Also commit the link value.

--- branches/OpenFDM-StateSeparation/src/OpenFDM/MechanicLinkValue.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/MechanicLinkValue.h
@@ -8,79 +8,9 @@
 #include "Inertia.h"
 #include "PortValue.h"
 #include "Frame.h"
+#include "Environment.h"
 
 namespace OpenFDM {
-
-class Environment;
-
-class AbstractInertial : public Referenced {
-public:
-  virtual ~AbstractInertial() {}
-  virtual Vector3 getAngularVelocity(const real_type& t) const
-  { return Vector3::zeros(); }
-//   { return Vector3(0, 0, pi2/(24*60*60)); }
-  virtual Vector6 getAcceleration(const real_type& t) const
-  { return Vector6::zeros(); }
-};
-
-class AbstractGravity : public Referenced {
-public:
-  virtual ~AbstractGravity() {}
-  virtual Vector3
-  getGravityAcceleration(const Environment&, const Vector3&) const
-  { return Vector3(0, 0, 9.81); }
-};
-
-class AbstractWind : public Referenced {
-public:
-  virtual ~AbstractWind() {}
-  virtual Vector6
-  getWindVelocity(const Environment&, const real_type& t, const Vector3&) const
-  { return Vector6::zeros(); }
-};
-
-class Environment : public Referenced {
-public:
-  Environment() :
-    mInertial(new AbstractInertial),
-    mGravity(new AbstractGravity),
-    mWind(new AbstractWind)
-  {
-  }
-  Environment(const AbstractInertial* inertial,
-              const AbstractGravity* gravity,
-              const AbstractWind* wind) :
-    mInertial(inertial),
-    mGravity(gravity),
-    mWind(wind)
-  {
-  }
-  virtual ~Environment() {}
-
-  // The the global coordinate frames angular velocity and acceleration.
-  // Note that the acceleration and velocity must fit together to simulate
-  // something useful.
-  Vector3 getAngularVelocity(const real_type& t) const
-  { return mInertial->getAngularVelocity(t); }
-  Vector6 getAcceleration(const real_type& t) const
-  { return mInertial->getAcceleration(t); }
-
-  // The gravity acceleration vector in the global coordinate system
-  Vector3 getGravityAcceleration(const Vector3& position) const
-  { return mGravity->getGravityAcceleration(*this, position); }
-
-  // The wind velocity vector in the global coordinate system
-  Vector6 getWindVelocity(const real_type& t, const Vector3& position) const
-  { return mWind->getWindVelocity(*this, t, position); }
-
-private:
-  SharedPtr<const AbstractInertial> mInertial;
-  SharedPtr<const AbstractGravity> mGravity;
-  SharedPtr<const AbstractWind> mWind;
-//   SharedPtr<const AbstractPlanet> mPlanet;
-//   SharedPtr<const AbstractAtmosphere> mAtmosphere;
-//   SharedPtr<const AbstractGround> mGround;
-};
 
 class EnvironmentCache : public Environment {
 public:
