0bf3b5d9ab64c8b2e647735f7db1981f32189ee7
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-11-24 06:12:10 +0000

    Update to match the other files. Duh, should now be again up to date past the git-svn problems ...
    M    Environment.cpp
    M    Environment.h

--- branches/OpenFDM-StateSeparation/src/OpenFDM/Environment.cpp
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/Environment.cpp
@@ -5,6 +5,7 @@
 #include "Environment.h"
 
 #include "FlatPlanet.h"
+#include "AtmosphereSTD1976.h"
 
 namespace OpenFDM {
 
@@ -12,7 +13,9 @@ Environment::Environment() :
   mInertial(new AbstractInertial),
   mGravity(new AbstractGravity),
   mWind(new AbstractWind),
-  mPlanet(new FlatPlanet)
+  mPlanet(new FlatPlanet),
+  mAtmosphere(new AtmosphereSTD1976),
+  mGround(new AbstractGround)
 {
 }
 
--- branches/OpenFDM-StateSeparation/src/OpenFDM/Environment.h
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/Environment.h
@@ -5,11 +5,14 @@
 #ifndef OpenFDM_Environment_H
 #define OpenFDM_Environment_H
 
+#include "AbstractAtmosphere.h"
 #include "AbstractGravity.h"
+#include "AbstractGround.h"
 #include "AbstractInertial.h"
 #include "AbstractPlanet.h"
 #include "AbstractWind.h"
 #include "Matrix.h"
+#include "Plane.h"
 #include "Referenced.h"
 #include "SharedPtr.h"
 #include "Vector.h"
@@ -21,6 +24,10 @@ public:
   Environment();
   virtual ~Environment();
 
+  ///////////////////////////////////////////////////////////////////////////
+  // Inertial related
+  // FIXME may be this can be split out???
+
   // The the global coordinate frames angular velocity and acceleration.
   // Note that the acceleration and velocity must fit together to simulate
   // something useful.
@@ -29,21 +36,70 @@ public:
   Vector6 getAcceleration(const real_type& t) const
   { return mInertial->getAcceleration(t); }
 
+  ///////////////////////////////////////////////////////////////////////////
+  // Gravity related
+
   // The gravity acceleration vector in the global coordinate system
   Vector3 getGravityAcceleration(const Vector3& position) const
   { return mGravity->getGravityAcceleration(*this, position); }
 
+  ///////////////////////////////////////////////////////////////////////////
+  // Wind sensing related
+
   // The wind velocity vector in the global coordinate system
   Vector6 getWindVelocity(const real_type& t, const Vector3& position) const
   { return mWind->getWindVelocity(*this, t, position); }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Planet related
+  // FIXME: find out what we really need here, may be some kind of
+  // abstract horizontal local coordinates?
+  // Is responsible for altitude computations ...
+
+  // From the planet these are so far what we need.
+  // FIXME, invent an abstraction for that, may be optimize???
+protected:
+  Vector3 getHorizontalLocalDown(const Vector3& position) const
+  {
+    Quaternion hlOr = mPlanet->getGeodHLOrientation(position);
+    return hlOr.backTransform(Vector3::unit(3));
+  }
+  Vector3 getHorizontalLocalOffset(const Vector3& position) const
+  {
+    Geodetic geod = mPlanet->toGeod(position);
+    geod.altitude = 0;
+    return mPlanet->toCart(geod);
+  }
+public:
+  Plane getHorizontalLocalPlane(const Vector3& position) const
+  {
+    // Get the unit down vector.
+    Vector3 unitDown = getHorizontalLocalDown(position);
+    // Get the distance from the planets center.
+    Vector3 groundOff = getHorizontalLocalOffset(position);
+    // Then we know the plane ...
+    return Plane(unitDown, groundOff);
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Atmosphere sensing related
+  AtmosphereData getAtmosphereData(const Vector3& position) const
+  {
+    Geodetic geod = mPlanet->toGeod(position);
+    return mAtmosphere->getData(geod.altitude);
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  // Intersection interaction related
+
 
 private:
   SharedPtr<const AbstractInertial> mInertial;
   SharedPtr<const AbstractGravity> mGravity;
   SharedPtr<const AbstractWind> mWind;
   SharedPtr<const AbstractPlanet> mPlanet;
-//   SharedPtr<const AbstractAtmosphere> mAtmosphere;
-//   SharedPtr<const AbstractGround> mGround;
+  SharedPtr<const AbstractAtmosphere> mAtmosphere;
+  SharedPtr<const AbstractGround> mGround;
 };
 
 } // namespace OpenFDM
