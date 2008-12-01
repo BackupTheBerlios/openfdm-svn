2d73788d537ab31dad8c496679aa73df459f55b2
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2008-12-01 21:43:21 +0000

    tiretestrig.
    M    src/test/tiretestrig.cpp
    M    src/test/Makefile.am

--- branches/OpenFDM-StateSeparation/src/test/tiretestrig.cpp
+++ branches/OpenFDM-StateSeparation/src/test/tiretestrig.cpp
@@ -6,13 +6,10 @@
 #include <OpenFDM/DoPri5.h>
 #include <OpenFDM/FixedRootJoint.h>
 #include <OpenFDM/Gain.h>
-#include <OpenFDM/Gravity.h>
-#include <OpenFDM/Ground.h>
 #include <OpenFDM/Integrator.h>
 #include <OpenFDM/LinearSpringDamper.h>
 #include <OpenFDM/Mass.h>
-#include <OpenFDM/ModelGroup.h>
-#include <OpenFDM/ModelVisitor.h>
+#include <OpenFDM/Group.h>
 #include <OpenFDM/Output.h>
 #include <OpenFDM/PrismaticJoint.h>
 #include <OpenFDM/RevoluteActuator.h>
@@ -21,18 +18,18 @@
 #include <OpenFDM/SimulationTime.h>
 #include <OpenFDM/Summer.h>
 #include <OpenFDM/System.h>
+#include <OpenFDM/SystemOutput.h>
 #include <OpenFDM/UnaryFunction.h>
 #include <OpenFDM/WheelContact.h>
 
-#include "HDF5Writer.h"
-
 using namespace OpenFDM;
 
-class MovingGround : public Ground {
+class MovingGround : public AbstractGround {
 public:
   MovingGround(const Vector3& linearVelocity) : mVelocity(linearVelocity)
   { }
-  virtual GroundValues getGroundPlane(real_type, const Vector3& refPos) const
+  virtual GroundValues getGroundPlane(const Environment&, const real_type& t,
+                                      const Vector3& refPos) const
   {
     return GroundValues(Plane(Vector3(0, 0, -1), Vector3::zeros()),
                         Vector6(Vector3::zeros(), mVelocity),
@@ -42,130 +39,119 @@ private:
   Vector3 mVelocity;
 };
 
-class ZGravity : public Gravity {
-public:
-  virtual Vector3 gravityAccel(const Vector3& cart) const
-  { return Vector3(0, 0, 9.81); }
-};
-
 int
 main(int argc, char *argv[])
 {
-  real_type omega = 2;
+  SharedPtr<Group> group = new Group("Tire Testrig");
 
-  SharedPtr<System> system = new System("Tire Testrig");
-  system->addSampleTime(real_type(1)/real_type(100));
-  system->setTimestepper(new DoPri5);
-
-  // set the moving ground
-  system->getEnvironment()->setGround(new MovingGround(Vector3(10, 0, 0)));
-  system->getEnvironment()->setGravity(new ZGravity());
-  
   // First build up the mechanical system
   FixedRootJoint* fixedRootJoint = new FixedRootJoint("Fixed Root Joint");
-  system->addModel(fixedRootJoint);
-  fixedRootJoint->setRefPosition(Vector3(0, 0, 1));
+  group->addChild(fixedRootJoint);
+  fixedRootJoint->setPosition(Vector3(0, 0, 1));
 
   PrismaticJoint* prismaticJoint = new PrismaticJoint("Normal Force joint");
-  prismaticJoint->setJointAxis(Vector3::unit(2));
-  system->addModel(prismaticJoint);
+  prismaticJoint->setAxis(Vector3::unit(2));
+  group->addChild(prismaticJoint);
 
   Summer* normalForceSum = new Summer("Normal Force Sum");
   normalForceSum->setNumSummands(2);
   normalForceSum->setInputSign(0, Summer::Minus);
-  system->addModel(normalForceSum);
-  Connection::connect(prismaticJoint->getInputPort(0),
-                      normalForceSum->getOutputPort(0));
+  group->addChild(normalForceSum);
+  group->connect(prismaticJoint->getPort("input"),
+                 normalForceSum->getPort("output"));
 
   ConstModel* normalForce = new ConstModel("Normal force");
   normalForce->setScalarValue(2000);
-  system->addModel(normalForce);
-  Connection::connect(normalForceSum->getInputPort(0),
-                      normalForce->getOutputPort(0));
+  group->addChild(normalForce);
+  group->connect(normalForceSum->getPort("input0"),
+                 normalForce->getPort("output"));
  
   LinearSpringDamper* strutDamper = new LinearSpringDamper("Strut Damper");
   strutDamper->setSpringConstant(0);
   strutDamper->setDamperConstant(-30);
-  system->addModel(strutDamper);
-  Connection::connect(normalForceSum->getInputPort(1),
-                      strutDamper->getOutputPort(0));
-  Connection::connect(strutDamper->getInputPort(0),
-                      prismaticJoint->getOutputPort(0));
-  Connection::connect(strutDamper->getInputPort(1),
-                      prismaticJoint->getOutputPort(1));
+  group->addChild(strutDamper);
+  group->connect(normalForceSum->getPort("input1"),
+                 strutDamper->getPort("force"));
+  group->connect(strutDamper->getPort("velocity"),
+                 prismaticJoint->getPort("velocity"));
+  group->connect(strutDamper->getPort("position"),
+                 prismaticJoint->getPort("position"));
 
   RigidBody* rootMount = new RigidBody("Root Mount");
-  system->addModel(rootMount);
-  rootMount->setInboardJoint(fixedRootJoint);
-  rootMount->addInteract(prismaticJoint);
-
+  group->addChild(rootMount);
+  group->connect(rootMount->getPort("link0"), fixedRootJoint->getPort("link"));
+  group->connect(rootMount->getPort("link1"), prismaticJoint->getPort("link0"));
 
   RevoluteActuator* camberActuator = new RevoluteActuator("Camber Actuator");
-  system->addModel(camberActuator);
+  group->addChild(camberActuator);
 
   ConstModel* camberAngle = new ConstModel("Camber Angle");
   camberAngle->setScalarValue(0);
-  system->addModel(camberAngle);
-  Connection::connect(camberActuator->getInputPort(0),
-                      camberAngle->getOutputPort(0));
+  group->addChild(camberAngle);
+  group->connect(camberActuator->getPort("input"),
+                 camberAngle->getPort("output"));
 
   RigidBody* normalForceStrut = new RigidBody("Normal Force Strut");
-  system->addModel(normalForceStrut);
-  normalForceStrut->setInboardJoint(prismaticJoint);
-  normalForceStrut->addInteract(camberActuator);
+  group->addChild(normalForceStrut);
+  group->connect(normalForceStrut->getPort("link0"),
+                 prismaticJoint->getPort("link1"));
+  group->connect(normalForceStrut->getPort("link1"),
+                 camberActuator->getPort("link0"));
 
  
   RevoluteActuator* sideActuator = new RevoluteActuator("Sideslip Actuator");
-  system->addModel(sideActuator);
+  group->addChild(sideActuator);
 
   ConstModel* sideslipAngle = new ConstModel("Sideslip Angle");
   sideslipAngle->setScalarValue(0);
-  system->addModel(sideslipAngle);
-  Connection::connect(sideActuator->getInputPort(0),
-                      sideslipAngle->getOutputPort(0));
+  group->addChild(sideslipAngle);
+  group->connect(sideActuator->getPort("input"),
+                 sideslipAngle->getPort("output"));
 
   RigidBody* camberStrut = new RigidBody("Camber Strut");
-  system->addModel(camberStrut);
-  camberStrut->setInboardJoint(camberActuator);
-  camberStrut->addInteract(sideActuator);
+  group->addChild(camberStrut);
+  group->connect(camberStrut->getPort("link0"),
+                 camberActuator->getPort("link1"));
+  group->connect(camberStrut->getPort("link1"), sideActuator->getPort("link0"));
 
 
   RevoluteJoint* hubJoint = new RevoluteJoint("Hub Joint");
-  hubJoint->setJointAxis(Vector3(0, 1, 0));
-  system->addModel(hubJoint);
+  hubJoint->setAxis(Vector3(0, 1, 0));
+  group->addChild(hubJoint);
 
   RigidBody* hubStrut = new RigidBody("Hub Strut");
-  system->addModel(hubStrut);
-  hubStrut->setInboardJoint(sideActuator);
-  hubStrut->addInteract(hubJoint);
+  group->addChild(hubStrut);
+  group->connect(hubStrut->getPort("link0"), sideActuator->getPort("link1"));
+  group->connect(hubStrut->getPort("link1"), hubJoint->getPort("link0"));
 
   RigidBody* rimAndTire = new RigidBody("Rim And Tire");
-  system->addModel(rimAndTire);
-  rimAndTire->setInboardJoint(hubJoint);
+  group->addChild(rimAndTire);
+  group->connect(hubJoint->getPort("link1"), rimAndTire->getPort("link0"));
 
   Mass* tireAndRimMass = new Mass("Rim And Tire Mass");
-  tireAndRimMass->setInertia(1, InertiaMatrix(1, 0, 0, 1, 0, 1));
-  system->addModel(tireAndRimMass);
-  rimAndTire->addInteract(tireAndRimMass);
+  tireAndRimMass->setMass(1);
+  tireAndRimMass->setInertia(InertiaMatrix(1, 0, 0, 1, 0, 1));
+  group->addChild(tireAndRimMass);
+  group->connect(rimAndTire->getPort("link1"), tireAndRimMass->getPort("link"));
   
   WheelContact* wheelContact = new WheelContact("Wheel Contact");
   wheelContact->setWheelRadius(0.3);
   wheelContact->setSpringConstant(1000);
   wheelContact->setSpringDamping(sqrt(wheelContact->getSpringConstant())/10);
-  system->addModel(wheelContact);
-  rimAndTire->addInteract(wheelContact);
+  group->addChild(wheelContact);
+  rimAndTire->addLink("link2");
+  group->connect(rimAndTire->getPort("link2"), wheelContact->getPort("link"));
 
-  if (!system->init()) {
-    std::cout << "Could not initialize the system" << std::endl;
-    return EXIT_FAILURE;
-  }
+  SharedPtr<System> system = new System("Tire Testrig", group);
 
-  HDF5Writer hwriter("system.h5");
-  system->accept(hwriter);
-  while (system->getTime() < 10) {
-    system->simulate(system->getTime() + 0.01);
-    system->accept(hwriter);
-  }
+  // set the moving ground
+  system->getEnvironment()->setGround(new MovingGround(Vector3(10, 0, 0)));
+
+  system->attach(SystemOutput::newDefaultSystemOutput("tiretestrig.h5"));
+  if (!system->init())
+    return 1;
+
+  system->simulate(60);
   
   return EXIT_SUCCESS;
 }
