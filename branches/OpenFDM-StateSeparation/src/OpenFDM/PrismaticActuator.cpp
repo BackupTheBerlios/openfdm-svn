39ccb8d6cd53b6aae4f3e0e7e39655f15ab6c3ed
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2009-01-17 21:34:44 +0000

    Add a prismatic actuator model.
    AM   PrismaticActuator.cpp
    AM   PrismaticActuator.h
    M    Makefile.am

---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/PrismaticActuator.cpp
@@ -0,0 +1,228 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2009 Mathias Froehlich 
+ *
+ */
+
+#include "PrismaticActuator.h"
+#include "Assert.h"
+#include "LogStream.h"
+#include "Limits.h"
+#include "Object.h"
+#include "Vector.h"
+#include "Matrix.h"
+#include "Quaternion.h"
+#include "Inertia.h"
+#include "PortValueList.h"
+#include "ContinousStateValueVector.h"
+#include "MechanicContext.h"
+
+namespace OpenFDM {
+
+BEGIN_OPENFDM_OBJECT_DEF(PrismaticActuator, Joint)
+  DEF_OPENFDM_PROPERTY(Vector3, Axis, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, InitialPosition, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, InitialVelocity, Serialized)
+  DEF_OPENFDM_PROPERTY(Bool, VelocityControl, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, MaxVel, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, VelGain, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, VelDotGain, Serialized)
+  END_OPENFDM_OBJECT_DEF
+
+PrismaticActuator::PrismaticActuator(const std::string& name) :
+  CartesianJoint<1>(name),
+  mInputPort(this, "input", Size(1, 1), true),
+  mPositionPort(this, "position", Size(1, 1)),
+  mVelocityPort(this, "velocity", Size(1, 1)),
+  mPositionStateInfo(new Vector1StateInfo),
+  mVelocityStateInfo(new Vector1StateInfo),
+  mAxis(Vector3(1, 0, 0)),
+  mInitialPosition(0),
+  mInitialVelocity(0),
+  mVelocityControl(false),
+  mVelGain(1),
+  mVelDotGain(1),
+  mMaxVel(1)
+{
+  addContinousStateInfo(mPositionStateInfo);
+  addContinousStateInfo(mVelocityStateInfo);
+
+  // FIXME
+  setAxis(mAxis);
+}
+
+PrismaticActuator::~PrismaticActuator(void)
+{
+}
+
+const Vector3&
+PrismaticActuator::getAxis() const
+{
+  return mAxis;
+}
+
+void
+PrismaticActuator::setAxis(const Vector3& axis)
+{
+  real_type nrm = norm(axis);
+  if (nrm <= Limits<real_type>::min()) {
+    Log(Initialization, Error) << "JointAxis is zero ..." << endl;
+    return;
+  }
+  mAxis = (1/nrm)*axis;
+}
+
+const real_type&
+PrismaticActuator::getInitialPosition() const
+{
+  return mInitialPosition;
+}
+
+void
+PrismaticActuator::setInitialPosition(const real_type& initialPosition)
+{
+  mInitialPosition = initialPosition;
+}
+
+const real_type&
+PrismaticActuator::getInitialVelocity() const
+{
+  return mInitialVelocity;
+}
+
+void
+PrismaticActuator::setInitialVelocity(const real_type& initialVelocity)
+{
+  mInitialVelocity = initialVelocity;
+}
+
+const bool&
+PrismaticActuator::getVelocityControl(void) const
+{
+  return mVelocityControl;
+}
+
+void
+PrismaticActuator::setVelocityControl(const bool& velocityControl)
+{
+  mVelocityControl = velocityControl;
+}
+
+const real_type&
+PrismaticActuator::getMaxVel(void) const
+{
+  return mMaxVel;
+}
+
+void
+PrismaticActuator::setMaxVel(const real_type& maxVel)
+{
+  mMaxVel = maxVel;
+}
+
+const real_type&
+PrismaticActuator::getVelGain(void) const
+{
+  return mVelGain;
+}
+
+void
+PrismaticActuator::setVelGain(const real_type& velGain)
+{
+  mVelGain = velGain;
+}
+
+const real_type&
+PrismaticActuator::getVelDotGain(void) const
+{
+  return mVelDotGain;
+}
+
+void
+PrismaticActuator::setVelDotGain(const real_type& velDotGain)
+{
+  mVelDotGain = velDotGain;
+}
+
+void
+PrismaticActuator::init(const Task&, DiscreteStateValueVector&,
+                       ContinousStateValueVector& continousState,
+                       const PortValueList&) const
+{
+  continousState[*mPositionStateInfo] = mInitialPosition;
+  continousState[*mVelocityStateInfo] = mInitialVelocity;
+}
+
+PrismaticActuator::Matrix6N
+PrismaticActuator::getJointMatrix() const
+{
+  return Vector6(Vector3::zeros(), mAxis);
+}
+
+void
+PrismaticActuator::velocity(const Task&, Context& context,
+                        const ContinousStateValueVector& states,
+                        PortValueList& portValues) const
+{
+  VectorN jointPos = states[*mPositionStateInfo];
+  if (!mPositionPort.empty())
+    portValues[mPositionPort] = jointPos;
+  
+  VectorN jointVel = states[*mVelocityStateInfo];
+  if (!mVelocityPort.empty())
+    portValues[mVelocityPort] = jointVel;
+  
+  context.setPosAndVel(mAxis*jointPos, Quaternion::unit(), jointVel);
+}
+
+void
+PrismaticActuator::articulation(const Task&, Context& context,
+                            const ContinousStateValueVector& states,
+                            PortValueList& portValues) const
+{
+  // This is a simple second order system with velocity limits.
+  // the joints accelerations, velocities and positions must fit together
+  // otherwise the articulated body dynamics get fooled ...
+
+  VectorN velDot;
+  if (!mVelocityControl) {
+    // The desired position input
+    VectorN desiredPos = portValues[mInputPort];
+    // Compute the error ...
+    VectorN posErr = desiredPos - states[*mPositionStateInfo];
+    // ... and compute a desired velocity within the given limits from that.
+    VectorN desiredVel;
+    desiredVel(0) = smoothSaturate(mVelGain*posErr(0), mMaxVel);
+    // The usual control loops: there we get a velocity error
+    VectorN velErr = desiredVel - states[*mVelocityStateInfo];
+    // and accelerate that proportional to that error ...
+    velDot = mVelDotGain*velErr;
+  } else {
+    // The desired velocity input
+    VectorN desiredVel = portValues[mInputPort];
+    // The usual control loops: there we get a velocity error
+    VectorN velErr = desiredVel - states[*mVelocityStateInfo];
+    // and accelerate that proportional to that error ...
+    velDot = mVelDotGain*velErr;
+  }
+
+  context.applyActuatorForce(velDot);
+}
+
+void
+PrismaticActuator::acceleration(const Task&, Context& context,
+                            const ContinousStateValueVector&,
+                            PortValueList&) const
+{
+  context.accelerateDueToVelDot();
+}
+
+void
+PrismaticActuator::derivative(const Task&, Context& context,
+                          const ContinousStateValueVector& states,
+                          const PortValueList&,
+                          ContinousStateValueVector& derivative) const
+{
+  derivative[*mPositionStateInfo] = states[*mVelocityStateInfo];
+  derivative[*mVelocityStateInfo] = context.getVelDot();
+}
+
+} // namespace OpenFDM
---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/PrismaticActuator.h
@@ -0,0 +1,94 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2009 Mathias Froehlich 
+ *
+ */
+
+#ifndef OpenFDM_PrismaticActuator_H
+#define OpenFDM_PrismaticActuator_H
+
+#include "Assert.h"
+#include "Object.h"
+#include "Vector.h"
+#include "Matrix.h"
+#include "MatrixStateInfo.h"
+#include "Quaternion.h"
+#include "Inertia.h"
+#include "Joint.h"
+#include "MatrixInputPort.h"
+#include "MatrixOutputPort.h"
+#include "ContinousStateValueVector.h"
+#include "PortValueList.h"
+#include "MechanicContext.h"
+#include "CartesianJoint.h"
+
+namespace OpenFDM {
+
+class PrismaticActuator : public CartesianJoint<1> {
+  OPENFDM_OBJECT(PrismaticActuator, Joint);
+public:
+  PrismaticActuator(const std::string& name);
+  virtual ~PrismaticActuator(void);
+
+  /** Sets the joint axis where this joint is allowed to rotate around.
+   */
+  const Vector3& getAxis() const;
+  void setAxis(const Vector3& axis);
+
+  const real_type& getInitialPosition() const;
+  void setInitialPosition(const real_type& initialPosition);
+
+  const real_type& getInitialVelocity() const;
+  void setInitialVelocity(const real_type& initialVelocity);
+
+  const bool& getVelocityControl(void) const;
+  void setVelocityControl(const bool& velocityControl);
+
+  const real_type& getMaxVel(void) const;
+  void setMaxVel(const real_type& maxVel);
+
+  const real_type& getVelGain(void) const;
+  void setVelGain(const real_type& velGain);
+
+  const real_type& getVelDotGain(void) const;
+  void setVelDotGain(const real_type& velDotGain);
+
+protected:
+
+  virtual void init(const Task&, DiscreteStateValueVector&,
+                    ContinousStateValueVector& continousState,
+                    const PortValueList&) const;
+  virtual Matrix6N getJointMatrix() const;
+
+  virtual void velocity(const Task& task, Context& context,
+                        const ContinousStateValueVector& states,
+                        PortValueList& portValues) const;
+  virtual void articulation(const Task& task, Context& context,
+                            const ContinousStateValueVector& states,
+                            PortValueList& portValues) const;
+  virtual void acceleration(const Task& task, Context& context,
+                            const ContinousStateValueVector& states,
+                            PortValueList& portValues) const;
+  virtual void derivative(const Task& task, Context& context,
+                          const ContinousStateValueVector&,
+                          const PortValueList& portValues,
+                          ContinousStateValueVector&) const;
+
+private:
+  MatrixInputPort mInputPort;
+  MatrixOutputPort mPositionPort;
+  MatrixOutputPort mVelocityPort;
+
+  SharedPtr<Vector1StateInfo> mPositionStateInfo;
+  SharedPtr<Vector1StateInfo> mVelocityStateInfo;
+
+  Vector3 mAxis;
+  real_type mInitialPosition;
+  real_type mInitialVelocity;
+  bool mVelocityControl;
+  real_type mVelGain;
+  real_type mVelDotGain;
+  real_type mMaxVel;
+};
+
+} // namespace OpenFDM
+
+#endif
