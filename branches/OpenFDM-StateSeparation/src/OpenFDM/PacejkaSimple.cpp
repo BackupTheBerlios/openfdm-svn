367ba77fd82e83701a4fda290f58d2d2087e9a98
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2009-03-20 19:04:48 +0000

    Add Simple Pacejka type shaped curves.
    M    OpenFDM/Makefile.am
    AM   OpenFDM/PacejkaSimple.cpp
    AM   OpenFDM/PacejkaSimple.h

---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/PacejkaSimple.cpp
@@ -0,0 +1,87 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2009 Mathias Froehlich 
+ *
+ */
+
+#include "PacejkaSimple.h"
+
+#include "TypeInfo.h"
+#include "Variant.h"
+
+namespace OpenFDM {
+
+BEGIN_OPENFDM_OBJECT_DEF(PacejkaSimple, PacejkaTire)
+  DEF_OPENFDM_PROPERTY(Real, SpringConstant, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, DampingConstant, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Bx, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Cx, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Dx, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Ex, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, By, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Cy, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Dy, Serialized)
+  DEF_OPENFDM_PROPERTY(Real, Ey, Serialized)
+  END_OPENFDM_OBJECT_DEF
+
+PacejkaSimple::PacejkaSimple(const std::string& name) :
+  PacejkaTire(name),
+  mSpringConstant(0),
+  mDampingConstant(0),
+  mBx(1), mCx(0.01), mDx(1), mEx(0),
+  mBy(10), mCy(5), mDy(1), mEy(0)
+{
+}
+
+PacejkaSimple::~PacejkaSimple(void)
+{
+}
+
+Vector6
+PacejkaSimple::getForce(const real_type& rho, const real_type& rhoDot,
+                        const real_type& alpha, const real_type& kappa,
+                        const real_type& gamma, const real_type& phi) const
+{
+  // The normal force
+  real_type Fz = rho*mSpringConstant + mDampingConstant*rhoDot;
+  // The normal force cannot get negative here.
+  Fz = max(real_type(0), Fz);
+
+  //
+  // Longitudinal force (Pure longitudinal slip)
+  //
+  real_type Bxk = mBx*100*kappa;
+  real_type Fx = Fz*mDx*sin(mCx*atan(Bxk - mEx*(Bxk - atan(Bxk))));
+
+  //
+  // Lateral force (Pure side slip)
+  //
+  real_type Bya = mBy*rad2deg*alpha;
+  real_type Fy = Fz*mDy*sin(mCy*atan(Bya - mEy*(Bya - atan(Bya))));
+
+  return Vector6(Vector3(0, 0, 0), Vector3(Fx, Fy, Fz));
+}
+
+void
+PacejkaSimple::setSpringConstant(const real_type& springConstant)
+{
+  mSpringConstant = springConstant;
+}
+
+const real_type&
+PacejkaSimple::getSpringConstant(void) const
+{
+  return mSpringConstant;
+}
+
+void
+PacejkaSimple::setDampingConstant(const real_type& dampingConstant)
+{
+  mDampingConstant = dampingConstant;
+}
+
+const real_type&
+PacejkaSimple::getDampingConstant(void) const
+{
+  return mDampingConstant;
+}
+
+} // namespace OpenFDM
---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/PacejkaSimple.h
@@ -0,0 +1,64 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2009 Mathias Froehlich 
+ *
+ */
+
+#ifndef OpenFDM_PacejkaSimple_H
+#define OpenFDM_PacejkaSimple_H
+
+#include "PacejkaTire.h"
+
+namespace OpenFDM {
+
+class PacejkaSimple : public PacejkaTire {
+  OPENFDM_OBJECT(PacejkaSimple, PacejkaTire);
+public:
+  PacejkaSimple(const std::string& name);
+  virtual ~PacejkaSimple(void);
+
+  // Helper function to compute the resulting force
+  virtual Vector6 getForce(const real_type& rho, const real_type& rhoDot,
+                           const real_type& alpha, const real_type& kappa,
+                           const real_type& gamma, const real_type& phi) const;
+
+  void setSpringConstant(const real_type& springConstant);
+  const real_type& getSpringConstant(void) const;
+
+  void setDampingConstant(const real_type& dampingConstant);
+  const real_type& getDampingConstant(void) const;
+
+  const real_type& getBx() const { return mBx; }
+  void setBx(const real_type& value) { mBx = value; }
+
+  const real_type& getCx() const { return mCx; }
+  void setCx(const real_type& value) { mCx = value; }
+
+  const real_type& getDx() const { return mDx; }
+  void setDx(const real_type& value) { mDx = value; }
+
+  const real_type& getEx() const { return mEx; }
+  void setEx(const real_type& value) { mEx = value; }
+
+
+  const real_type& getBy() const { return mBy; }
+  void setBy(const real_type& value) { mBy = value; }
+
+  const real_type& getCy() const { return mCy; }
+  void setCy(const real_type& value) { mCy = value; }
+
+  const real_type& getDy() const { return mDy; }
+  void setDy(const real_type& value) { mDy = value; }
+
+  const real_type& getEy() const { return mEy; }
+  void setEy(const real_type& value) { mEy = value; }
+
+private:
+  real_type mSpringConstant;
+  real_type mDampingConstant;
+
+  real_type mBx, mCx, mDx, mEx;
+  real_type mBy, mCy, mDy, mEy;
+};
+
+} // namespace OpenFDM
+
+#endif
