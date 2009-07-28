41f29ab20e4b8528a51bc6273a03be23fbc3e9f9
frohlich <frohlich@63721e7d-bf01-0410-baba-fa17cfcce4f1>
2009-07-28 05:34:00 +0000

    Add a division by zero safe 1/x block.
    M    Makefile.am
    AM   SafeReciprocal.cpp
    AM   SafeReciprocal.h

---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/SafeReciprocal.cpp
@@ -0,0 +1,42 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2009 Mathias Froehlich 
+ *
+ */
+
+#include "SafeReciprocal.h"
+
+#include "TypeInfo.h"
+#include "Variant.h"
+
+namespace OpenFDM {
+
+BEGIN_OPENFDM_OBJECT_DEF(SafeReciprocal, SimpleDirectModel)
+  DEF_OPENFDM_PROPERTY(Real, Epsilon, Serialized)
+  END_OPENFDM_OBJECT_DEF
+
+SafeReciprocal::SafeReciprocal(const std::string& name, const real_type& eps) :
+  SimpleDirectModel(name),
+  mEpsilon(eps)
+{
+  addInputPort("input");
+}
+
+SafeReciprocal::~SafeReciprocal(void)
+{
+}
+
+void
+SafeReciprocal::output(Context& context) const
+{
+  // FIXME, optimize, move that into a proper context ...
+  // For now make it work
+  Size sz = size(context.getInputValue(0));
+  for (unsigned j = 0; j < sz(1); ++j) {
+    for (unsigned i = 0; i < sz(0); ++i) {
+      real_type value = context.getInputValue(0)(i, j);
+      real_type rvalue = copysign(real_type(1)/(fabs(value) + mEpsilon), value);
+      context.getOutputValue()(i, j) = rvalue;
+    }
+  }
+}
+
+} // namespace OpenFDM
---
+++ branches/OpenFDM-StateSeparation/src/OpenFDM/SafeReciprocal.h
@@ -0,0 +1,33 @@
+/* -*-c++-*- OpenFDM - Copyright (C) 2004-2009 Mathias Froehlich 
+ *
+ */
+
+#ifndef OpenFDM_SafeReciprocal_H
+#define OpenFDM_SafeReciprocal_H
+
+#include <string>
+#include "SimpleDirectModel.h"
+
+namespace OpenFDM {
+
+class SafeReciprocal : public SimpleDirectModel {
+  OPENFDM_OBJECT(SafeReciprocal, SimpleDirectModel);
+public:
+  SafeReciprocal(const std::string& name,
+                 const real_type& epsilon = Limits<real_type>::safe_min());
+  virtual ~SafeReciprocal(void);
+
+  void output(Context& context) const;
+
+  const real_type& getEpsilon() const
+  { return mEpsilon; }
+  void setEpsilon(const real_type& epsilon)
+  { mEpsilon = epsilon; }
+
+private:
+  real_type mEpsilon;
+};
+
+} // namespace OpenFDM
+
+#endif
